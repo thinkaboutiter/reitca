6c98a793ccb376a0e02a3409313d44fa30df40a8
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-06-28 23:55:25 +0300

    add initial project

---
+++ salary-calculator/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BulgariaSalaryCalculator from './components/BulgariaSalaryCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BulgariaSalaryCalculator />
+    </div>
+  );
+}
+
+export default App;
